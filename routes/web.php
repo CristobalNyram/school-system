8a691aaad8643333ef2cee562b9380dba6227be1
cristobal nyram <crisco.nyram@gmail.com>
2022-08-02 08:38:44 -0500

    edit

--- routes/web.php
+++ routes/web.php
@@ -11,50 +11,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,"index"]);
 
-#Rutas para el apartado de encuesta
-Route::prefix('encuesta')->group(function () {
-    //Ruta para mostrar informacion al alumno.
-    Route::get('/', [EncuestaController::class,"index"]);
-    //Ruta para solicitar informacion basica al alumno.
-    Route::get('/info',[EncuestaController::class,"info_alumno"])->name('info');
-    
-    //ruta para enviar los datos del alumno
-    Route::post('/save',[EncuestaController::class,"save_alumno"])->name('save_alumno');
-    Route::post('/save_encuesta',[EncuestaController::class,"save_encuesta"])->name('save_encuesta');
-    Route::post('/save_comprobante',[EncuestaController::class,"save_comprobante"])->name('save_comprobante');
-
-    //Ruta para que el alumno conteste el cuestrioario.
-    Route::get('/info/{matricula}/{folio}',[EncuestaController::class,"show_encuesta"])
-        ->where('matricula','[0-9]+')
-        ->where('folio','[0-9]+');
-    
-    //Ruta para entregar PDF de resultados al alumno.
-    Route::get('/resultados/pdf/{matricula}/{folio}',[EncuestaController::class,"show_pdf_resultados"])
-        ->where('matricula','[0-9]+')
-        ->where('folio','[0-9]+');
-
-    //Ruta para solicitar la matricula para descargar PDF
-    Route::get('/pdf',[EncuestaController::class,"descargar_pdf_matricula"])->name('dowloadPDFmatricula');
-
-    //Ruta para descargar PDF con matricula
-    Route::get('/resultados/pdf/{matricula}/{telefono}',[EncuestaController::class,"show_pdf_resultados_matricula"])
-        ->where('matricula','[0-9]+');
-    
-});
-
-#Rutas para el apartado de verificación.
-Route::prefix('verificacion')->group(function () {
-    //Ruta de inicio para la verificacion (Policia)
-    Route::get('/', [VerificacionController::class,"index"])->name('verificacion');
-    //Ruta para verificar con QR.
-    Route::get('/qr', [VerificacionController::class,"show_qr"]);
-    Route::post('/show_info_qr', [VerificacionController::class,"show_info_qr"])->name('show_info_qr');
-    //Ruta para verificar con MATRICULA.
-    Route::get('/matricula', [VerificacionController::class,"show_matricula"]);
-    Route::get('/datosLocales', [VerificacionController::class,"datosEncuestas"])->name('datosLocales');
-    //Ruta para verificar con CURP.
-    Route::get('/curp', [VerificacionController::class,"show_curp"]);
-});
 
 #rutas para el apartado de login(Medico).
 Route::prefix('admin')->group(function () {
